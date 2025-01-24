f81db120e210fae78545b399069b08ad5d1d6c53
Keenan Klinkenborg <keenan.klinkenborg@jacks.sdstate.edu>
2025-01-24 10:49:59 -0600

    Added task 1

--- Programs/A0/A0Klinkenborg.cpp
+++ Programs/A0/A0Klinkenborg.cpp
@@ -37,6 +37,72 @@ void insertNode(node *&head, int index, int data)
     }
 }
 
+void deleteNode(node *&head, int index)
+{
+    // If the list is empty
+    if (head == nullptr)
+    {
+        cout << "List is empty, nothing to delete." << endl;
+        return;
+    }
+
+    // Deleting the first node (index 0)
+    if (index == 0)
+    {
+        node *temp = head;
+        head = head->next; // Move the head to the next node
+        delete temp; // Free the memory of the deleted node
+        cout << "Node at index 0 deleted." << endl;
+        return;
+    }
+
+    // Traverse to the node before the one to delete
+    node *walker = head;
+    int wIndex = 0;
+
+    while (walker != nullptr && wIndex < index - 1)
+    {
+        walker = walker->next;
+        wIndex++;
+    }
+
+    // If the index is out of range
+    if (walker == nullptr || walker->next == nullptr)
+    {
+        cout << "Index out of range. Deletion failed." << endl;
+        return;
+    }
+
+    // Delete the node at the specified index
+    node *temp = walker->next;
+    walker->next = temp->next; // Link the previous node to the next node
+    delete temp; // Free the memory of the deleted node
+    cout << "Node at index " << index << " deleted." << endl;
+}
+
+node* searchNode(node *head, int key)
+{
+    node *walker = head;
+    int index = 0;
+
+    // Traverse the list to find the key
+    while (walker != nullptr)
+    {
+        if (walker->data == key) // Key found
+        {
+            cout << "Key " << key << " found at index " << index << "." << endl;
+            return walker;
+        }
+        walker = walker->next;
+        index++;
+    }
+
+    // Key not found
+    cout << "Key " << key << " not found in the list." << endl;
+    return nullptr;
+}
+
+
 void displayList(node *head)
 {
     node* walker = head;
--- Programs/A0/main.cpp
+++ Programs/A0/main.cpp
@@ -7,15 +7,49 @@ int main()
 
     int choice, data, index;
 
-    cout << "Enter choice between 1-4: ";
-    cin >> choice;
+    do
+    {
+        cout << "Menu:\n";
+        cout << "1. Insert Node\n";
+        cout << "2. Delete Node\n";
+        cout << "3. Search Node\n";
+        cout << "4. Display List\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    //do{
+        switch (choice)
+        {
+            case 1:
+                cout << "Enter index and data to insert: ";
+                cin >> index >> data;
+                insertNode(head, index, data);
+                break;
 
+            case 2:
+                cout << "Enter index to delete: ";
+                cin >> index;
+                deleteNode(head, index);
+                break;
 
-    //}while(choice != 5);
+            case 3:
+                cout << "Enter key to search: ";
+                cin >> data;
+                searchNode(head, data);
+                break;
 
-    cout << "Code ran" << endl;
+            case 4:
+                displayList(head);
+                break;
 
+            case 5:
+                cout << "Exiting program." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice. Try again." << endl;
+        }
+    } while (choice != 5);
+    
     return 0;
 }
